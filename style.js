b682670ee45478c8aa03acac0d4b2406f0b64834
unknown <cda604@AFPA-PARIS-DI>
2021-01-13 17:18:34 +0100

    Javascript

---
+++ style.js
@@ -0,0 +1,14 @@
+	var nombre = prompt('Composez un numéro');
+	console.log (nombre);
+	if (nombre == 17) { alert("Vous souhaitez contacter la police")}
+	else if (nombre ==18) {alert ("Vous souhaitez contacter les pompiers")}
+	else {alert("Numéro inconnu")}
+
+
+	var prenom = ['Moussa', 'Noémie', 'Afpa', 'Vanessa']
+
+console.log ("Taille de mon tableau"+prenom.length)
+console.log(prenom[0])
+console.log(prenom[1])
+console.log(prenom[2])
+
